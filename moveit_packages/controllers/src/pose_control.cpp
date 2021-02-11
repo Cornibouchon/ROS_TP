d9f612674c4c865ee61459e40b927e92dcd78535
Cornibouchon <patrice.go@gmx.ch>
2021-02-11 17:24:29 +0100

    GripX Orientation

--- moveit_packages/controllers/src/pose_control.cpp
+++ moveit_packages/controllers/src/pose_control.cpp
@@ -15,7 +15,10 @@
 
 #include <sstream>
 #include <iostream>
+#include <math.h>
 using namespace std;
+#include <stdio.h>  
+#define PI 3.14159265
 
 
 int main(int argc, char** argv)
@@ -61,46 +64,59 @@ int main(int argc, char** argv)
 
 
   // Quaternion transformation
-  tf2::Quaternion myQuaternion;
-  myQuaternion.setRPY( -1.57, 0, 0 );  // Create this quaternion from roll/pitch/yaw (in radians)
-  // Normalize to 1
-  myQuaternion.normalize();
+  tf2::Quaternion myQuaternion, q_rot, q_new;
+  
 
   // Create a target pose
   geometry_msgs::Pose target_pose1;
 
-  //Transfer the values of of the created Quaternion
-  target_pose1.orientation.x = myQuaternion[0];
-  target_pose1.orientation.y = myQuaternion[1];
-  target_pose1.orientation.z = myQuaternion[2];
-  target_pose1.orientation.w = myQuaternion[3]; 
-
-
+  // Define all variables needed within the loop
+  double yaw = 0.0;
    int count = 0;
 
   while (ros::ok())
   {
+    // Set a faster
+    move_group.setMaxVelocityScalingFactor(1);
     cout << "Enter: X: Y: Z:" <<endl;
     cin >> target_pose1.position.x >>target_pose1.position.y >> target_pose1.position.z;
 
-    // target_pose1.orientation.w = 1;
-    // move_group.setPlanningTime(10.0);
-    // target_pose1.position.x = 0.4;
-    // target_pose1.position.y = 0.3;
-    // target_pose1.position.z = 0.5;
+    // Calculate the needed yaw angle needed from the coordinates
+    yaw = atan2(target_pose1.position.y, target_pose1.position.x);
+    //Print to check
+    printf("the arc tangent is %f \n", yaw);
+
+    //Calculate the quaternions
+    myQuaternion.setRPY( -1.5707, 0, 0 );  // Create this quaternion from roll/pitch/yaw (in radians)
+    q_rot.setRPY(0, yaw, 0);
+
+    //Calculate the new orientation
+    myQuaternion = myQuaternion * q_rot;
+
+    // Normalize to 1
+    myQuaternion.normalize();
+    // target_pose1.orientation= tf2::toMsg(myQuaternion);
+
+    //Transfer the values of of the created Quaternion
+    target_pose1.orientation.x = myQuaternion[0];
+    target_pose1.orientation.y = myQuaternion[1];
+    target_pose1.orientation.z = myQuaternion[2];
+    target_pose1.orientation.w = myQuaternion[3]; 
+
 
     // move_group.setOrientationTarget( myQuaternion[0], myQuaternion[1], myQuaternion[2], myQuaternion[3],
     //   move_group.getEndEffectorLink());
     // move_group.setGoalOrientationTolerance(1);
     move_group.setPoseTarget(target_pose1, move_group.getEndEffectorLink());
     // move_group.setApproximateJointValueTarget(target_pose1, move_group.getEndEffectorLink());
-    cout << count;
+    printf("Moving to %i target \n", count);
     move_group.move();
     ++count;
     // start_state.setFromIK(joint_model_group, target_pose1);
     // move_group.setStartState(start_state);
 
      // Planning a Cartesian Path by interpolating through waypoints
+
 
     //Create a vector of poses
     std::vector<geometry_msgs::Pose> waypoints;
@@ -110,13 +126,13 @@ int main(int argc, char** argv)
 
     geometry_msgs::Pose target_pose = target_pose1;
 
-    target_pose.position.z += 0.1;
+    target_pose.position.z += 0.05;
     waypoints.push_back(target_pose);  // down
 
-    target_pose.position.z += 0.1;
+    target_pose.position.z += 0.05;
     waypoints.push_back(target_pose);  // down
 
-    target_pose.position.z += 0.1;
+    target_pose.position.z += 0.05;
     waypoints.push_back(target_pose);  // up and left
 
     // Set a slower execution speed
